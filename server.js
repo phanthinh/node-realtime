51b16d6be3ce8f852714e146a120250adbbc944a
Phan Thinh <quocthinh9889@gmail.com>
2015-09-10 01:05:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
-var express = require("express");
-
-var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/chat');
-
+var express = require('express');
 var app = express();
-var port = 3700;
-
-
-require('./app/models/message');
-
-var Message = mongoose.model('Message');
-
-
-
-app.get("/", function(req, res){
-    res.send("It works!");
+//var path    = require("path");
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+ // res.sendFile(path.join(__dirname+'/index.html'));
 });
- 
-// app.listen(port);
+var port = 3000;
+/**
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+**/
+
 
 var io = require('socket.io').listen(app.listen(port));
-
+//io.set('transports', ["websocket", "polling"]);
 console.log('Chat server is running and listening to port %d... ',port);
 
 
@@ -28,9 +25,8 @@ io.sockets.on('connection', function (socket) {
     // socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
         // io.sockets.emit('message', data);
-		io.sockets.emit('message', data);
+                io.sockets.emit('message', data);
     });
-	
-	
-});
 
+
+});
